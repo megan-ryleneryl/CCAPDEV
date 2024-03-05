6ab04957b4076c350f79fe623670283ca2c459c4
Megan Sioco <megan_rylene_sioco@dlsu.edu.ph>
2024-03-05 09:05:10 +0800

    Completed registration

---
+++ public/js/register.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("upload-pfp").addEventListener("click", function() {
+        document.getElementById("file-input").click();
+    });
+});
